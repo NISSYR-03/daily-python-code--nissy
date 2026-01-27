e6f5464a3dd9ffedbae0c8bc0918f2c94ea1a86a
NISSY R <nissyr0306@gmail.com>
2026-01-27 23:15:23 +0530

    Day 7: Number guessing game mini project

---
+++ day7.py
@@ -0,0 +1,14 @@
+import random
+
+secret = random.randint(1, 10)
+guess = 0
+
+while guess != secret:
+    guess = int(input("Guess a number between 1 and 10: "))
+    
+    if guess < secret:
+        print("Too low!")
+    elif guess > secret:
+        print("Too high!")
+    else:
+        print("🎉 Correct! You guessed it.")
